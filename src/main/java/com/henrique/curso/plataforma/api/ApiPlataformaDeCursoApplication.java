4733a9451060564dbe9970c0df5b40c9953a08b8
carlos-henriquep <krlos.henrique98@gmail.com>
2024-04-23 11:46:36 -0300

    api-project firts commit

---
+++ src/main/java/com/henrique/curso/plataforma/api/ApiPlataformaDeCursoApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.curso.plataforma.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlataformaDeCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlataformaDeCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique/curso/plataforma/api/ApiPlataformaDeCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.curso.plataforma.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlataformaDeCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
